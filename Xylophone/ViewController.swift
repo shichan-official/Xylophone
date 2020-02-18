f9587f4a8fd463c16bdf7ccb9acf0c0db5af1704
Siavash <svshjalali@gmail.com>
2020-02-18 23:24:39 +0900

    Initial and probably last commit
    Simple app to work with basic sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.alpha = 0.75
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            sender.alpha = 1
+        }
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
